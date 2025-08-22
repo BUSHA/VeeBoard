72ede28aa7d6aef4c7351f8036b2e9ba358f0efa
Mykhailo <busha3@gmail.com>
2025-08-22 13:17:26 +0300

    Notification is clickable now

--- service-worker.js
+++ service-worker.js
@@ -25,3 +25,21 @@ self.addEventListener("message", (event) => {
     }
   }
 })
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close()
+
+  event.waitUntil(
+    clients
+      .matchAll({ type: "window", includeUncontrolled: true })
+      .then((clientList) => {
+        if (clientList.length > 0) {
+          return clientList[0].focus()
+        }
+
+        if (clients.openWindow) {
+          return clients.openWindow("/")
+        }
+      })
+  )
+})
